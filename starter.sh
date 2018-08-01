2df2ad73bc208f0201b641eb9788929d048850b2
Dean Christian <dean@unnotech.com>
2018-08-01 21:09:05 +0800

    base starter.sh

--- starter.sh
+++ starter.sh
@@ -1,7 +1,14 @@
 #!/bin/bash  #general
 docker login || exit 1  #general
+
+git submodule init
+git submodule update --init --remote
+
 docker-compose pull  #dev
 docker-compose up -d  #dev
+
+docker-compose exec angularjs npm install  #dev
+
 docker-compose exec django python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'pass1234')"  #dev
 docker-compose exec angularjs ng build --prod --build-optimizer  #dev
 docker-compose restart nginx  #dev
